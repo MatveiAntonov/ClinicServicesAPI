c25e7c4bca203cc2e14b6559b037e6caefb0d1b6
ProsTuha <tuhalf2002@mail.ru>
2023-02-09 16:19:51 +0300

    Allowed null-values for some properties

--- Management.Domain/Entities/Service.cs
+++ Management.Domain/Entities/Service.cs
@@ -8,10 +8,10 @@ namespace Management.Domain.Entities {
     public class Service {
         public int ServiceId { get; set; }
         public int ServiceCategoryId { get; set; }
-        public ServiceCategory ServiceCategory { get; set; } = new();
+        public ServiceCategory? ServiceCategory { get; set; } = new();
         public string ServiceName { get; set; } = string.Empty;
         public double? ServicePrice { get; set; }
         public int? SpecializationId { get; set; }
-        public Specialization Specialization { get; set; } = new();
+        public Specialization? Specialization { get; set; } = new();
     }
 }
