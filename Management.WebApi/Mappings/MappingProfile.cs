601c555deb070f90edb78c5d4f585eb7ddc46796
ProsTuha <tuhalf2002@mail.ru>
2023-02-17 16:17:24 +0300

    Some fixes

--- Management.WebApi/Mappings/MappingProfile.cs
+++ Management.WebApi/Mappings/MappingProfile.cs
@@ -5,22 +5,23 @@ using Management.Application.Services.Commands.CommandTypes;
 using Management.Application.ServiceCategories.Commands.CommandTypes;
 using Management.Application.Specializations.Commands.CommandTypes;
 
-namespace Management.WebApi.Mappings; 
-public class MappingProfile : Profile {
-    public MappingProfile()
-    {
-        CreateMap<Service, ServiceDto>()
-            .ForMember(dest => dest.ServiceCategoryName, opt => opt.MapFrom(src => src.ServiceCategory.ServiceCategoryName))
-            .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.Specialization.SpecializationName));
-        CreateMap<CreateServiceCommand, Service>();
-        CreateMap<UpdateServiceCommand, Service>();
+namespace Management.WebApi.Mappings {
+    public class MappingProfile : Profile {
+        public MappingProfile()
+        {
+            CreateMap<Service, ServiceDto>()
+                .ForMember(dest => dest.ServiceCategoryName, opt => opt.MapFrom(src => src.ServiceCategory.ServiceCategoryName))
+                .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.Specialization.SpecializationName));
+            CreateMap<CreateServiceCommand, Service>();
+            CreateMap<UpdateServiceCommand, Service>();
 
-        CreateMap<ServiceCategory, ServiceCategoryDto>();
-        CreateMap<CreateServiceCategoryCommand, ServiceCategory>();
-        CreateMap<UpdateServiceCategoryCommand, ServiceCategory>();
+            CreateMap<ServiceCategory, ServiceCategoryDto>();
+            CreateMap<CreateServiceCategoryCommand, ServiceCategory>();
+            CreateMap<UpdateServiceCategoryCommand, ServiceCategory>();
 
-        CreateMap<Specialization, SpecializationDto>();
-        CreateMap<CreateSpecializationCommand, Specialization>();
-        CreateMap<UpdateSpecializationCommand, Specialization>();
+            CreateMap<Specialization, SpecializationDto>();
+            CreateMap<CreateSpecializationCommand, Specialization>();
+            CreateMap<UpdateSpecializationCommand, Specialization>();
+        }
     }
 }
