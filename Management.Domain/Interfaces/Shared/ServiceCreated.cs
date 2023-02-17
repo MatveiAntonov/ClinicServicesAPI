defa3819979cd15f8e1fb94370ccb6f1b14092d3
ProsTuha <tuhalf2002@mail.ru>
2023-02-17 16:15:09 +0300

    Added interface for message for message broker

---
+++ Management.Domain/Interfaces/Shared/ServiceCreated.cs
@@ -0,0 +1,11 @@
+﻿namespace Management.Domain.Interfaces.Shared
+{
+    public interface ServiceCreated
+    {
+        public string ServiceCategoryName { get; set; }
+        public string ServiceName { get; set; }
+        public double? ServicePrice { get; set; }
+        public string? SpecializationName { get; set; }
+        public DateTime? TimeSlotSize { get; set; }
+    }
+}
