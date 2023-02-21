ac492fed1194d8ae289f1d85b8d2c1524dc4b966
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:19:34 +0300

    Added publisher messages for RabbitMq

---
+++ Management.Application/Publisher/Messages/ServiceCreated.cs
@@ -0,0 +1,12 @@
+﻿namespace Events
+{
+    public class ServiceCreated
+    {
+        public int Id { get; set; }
+        public string ServiceCategoryName { get; set; } = String.Empty;
+        public string ServiceName { get; set; }
+        public double? ServicePrice { get; set; }
+        public string SpecializationName { get; set; }
+        public DateTime? TimeSlotSize { get; set; }
+    }
+}
---
+++ Management.Application/Publisher/Messages/ServiceDeleted.cs
@@ -0,0 +1,12 @@
+﻿namespace Events
+{
+    public class ServiceDeleted
+    {
+        public int Id { get; set; }
+        public string ServiceCategoryName { get; set; } = String.Empty;
+        public string ServiceName { get; set; } = String.Empty;
+        public double? ServicePrice { get; set; }
+        public string SpecializationName { get; set; } = String.Empty;
+        public DateTime? TimeSlotSize { get; set; }
+    }
+}
